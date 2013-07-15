c2b5a77443a3973a3113b34cadef2d45d72186c2
SHIMADA Keiki <shimada.cake@gmail.com>
2013-07-15 21:15:19 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Setsuden::Application.config.secret_key_base = '27ea8bdf0e829f18747070b54240b08ce1c5846c34cc76d066b80ca119b6eb4bf045518dbe5d2c48e825004dc935b4bb1e7994c7c991fa243adfa48a08f035c1'
